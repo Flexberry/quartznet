9fe095f2bac8bc0cb780f4ed987f82438a114dc9
bkarabanov <bkarabanov@ics.perm.ru>
2017-12-22 21:35:24 +0500

    Convert  between IANA and Windows time zone names.

--- src/Quartz/Util/TimeZoneUtil.cs
+++ src/Quartz/Util/TimeZoneUtil.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Common.Logging;
+using NodaTime.TimeZones;
 
 namespace Quartz.Util
 {
@@ -93,6 +94,47 @@ namespace Quartz.Util
             return offset;
         }
 
+        // This will return the Windows zone that matches the IANA zone, if one exists.
+        public static string IanaToWindows(string ianaZoneId)
+        {
+            var utcZones = new[] { "Etc/UTC", "Etc/UCT", "Etc/GMT" };
+            if (utcZones.Contains(ianaZoneId, StringComparer.Ordinal))
+                return "UTC";
+
+            var tzdbSource = TzdbDateTimeZoneSource.Default;
+
+            // resolve any link, since the CLDR doesn't necessarily use canonical IDs
+            var links = tzdbSource.CanonicalIdMap
+                .Where(x => x.Value.Equals(ianaZoneId, StringComparison.Ordinal))
+                .Select(x => x.Key);
+
+            // resolve canonical zones, and include original zone as well
+            var possibleZones = tzdbSource.CanonicalIdMap.ContainsKey(ianaZoneId)
+                ? links.Concat(new[] { tzdbSource.CanonicalIdMap[ianaZoneId], ianaZoneId })
+                : links;
+
+            // map the windows zone
+            var mappings = tzdbSource.WindowsMapping.MapZones;
+            var item = mappings.FirstOrDefault(x => x.TzdbIds.Any(possibleZones.Contains));
+            if (item == null) return null;
+            return item.WindowsId;
+        }
+
+        // This will return the "primary" IANA zone that matches the given windows zone.
+        // If the primary zone is a link, it then resolves it to the canonical ID.
+        public static string WindowsToIana(string windowsZoneId)
+        {
+            if (windowsZoneId.Equals("UTC", StringComparison.Ordinal))
+                return "Etc/UTC";
+
+            var tzdbSource = NodaTime.TimeZones.TzdbDateTimeZoneSource.Default;
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(windowsZoneId);
+            if (tzi == null) return null;
+            var tzid = tzdbSource.MapTimeZoneId(tzi);
+            if (tzid == null) return null;
+            return tzdbSource.CanonicalIdMap[tzid];
+        }
+
         /// <summary>
         /// Tries to find time zone with given id, has ability do some fallbacks when necessary.
         /// </summary>
@@ -108,15 +150,23 @@ namespace Quartz.Util
             catch (TimeZoneNotFoundException)
             {
                 string aliasedId;
-                if (timeZoneIdAliases.TryGetValue(id, out aliasedId))
+                try
                 {
-                    try
+                    aliasedId = IanaToWindows(id) ?? WindowsToIana(id);
+                    info = TimeZoneInfo.FindSystemTimeZoneById(aliasedId);
+                }
+                catch
+                {
+                    if (timeZoneIdAliases.TryGetValue(id, out aliasedId))
                     {
-                        info = TimeZoneInfo.FindSystemTimeZoneById(aliasedId);
-                    }
-                    catch
-                    {
-                        logger.ErrorFormat("Could not find time zone using alias id " + aliasedId);
+                        try
+                        {
+                            info = TimeZoneInfo.FindSystemTimeZoneById(aliasedId);
+                        }
+                        catch
+                        {
+                            logger.ErrorFormat("Could not find time zone using alias id " + aliasedId);
+                        }
                     }
                 }
 
